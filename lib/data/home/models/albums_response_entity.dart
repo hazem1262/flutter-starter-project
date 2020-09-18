4bcfba93eae29fe1a17cafff915139704eb5bd9d
hazem ashraf <hazem.ashraf@ibtikar.net.sa>
2020-09-18 16:39:58 +0200

    [add] use json to dart bean factory to generate models

---
+++ lib/data/home/models/albums_response_entity.dart
@@ -0,0 +1,7 @@
+import 'package:flutterstarterproject/generated/json/base/json_convert_content.dart';
+
+class AlbumsResponseEntity with JsonConvert<AlbumsResponseEntity> {
+	double userId;
+	double id;
+	String title;
+}
