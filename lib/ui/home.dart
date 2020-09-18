6e328d22b8e71e78165af7e5c5f9e53bb0ad7c12
hazem ashraf <hazem.ashraf@ibtikar.net.sa>
2020-09-18 14:14:19 +0200

    [add] parse the response and display data

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -5,15 +5,6 @@ import 'package:http/http.dart' as http;
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -21,78 +12,46 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getPosts();
-  }
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: FutureBuilder<AlbumsResponse>(
+        future: getPosts(),
+        builder: (buildContext, snapshot){
+          if(snapshot.hasData){
+            var response = snapshot.data;
+            return ListView.builder(
+              itemCount: response.albums.length,
+              itemBuilder: (buildContext, index){
+                var album = response.albums[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(album.title, textAlign: TextAlign.center,),
+                    ),
+                  ),
+                );
+              }
+            );
+          } else if (snapshot.hasError){
+            return Center(child: Text("Networking error"),);
+          }
+          return Center(child: CircularProgressIndicator(),);
+        },
+      )
     );
   }
 
-  getPosts() async{
+  Future<AlbumsResponse> getPosts() async{
     var response = await http.get("http://jsonplaceholder.typicode.com/albums");
     print("status code: ${response.statusCode}");
     if (response.statusCode == 200) {
@@ -100,9 +59,52 @@ class _MyHomePageState extends State<MyHomePage> {
       var userId = jsonResponse[0]['userId'];
       var userTitle = jsonResponse[0]['title'];
       print('userId: $userId, userTitle:$userTitle');
+      AlbumsResponse albums = AlbumsResponse.fromJson(jsonResponse);
+      return albums;
     } else {
       print('Request failed with status: ${response.statusCode}.');
+      return Future.error(Exception("network error"));
     }
   }
 
+  void getAlbums() async{
+    var albums = await getPosts();
+    print("albums count is : ${albums.albums.length}");
+  }
+
+}
+
+class Album{
+  final int id;
+  final int userId;
+  final String title;
+
+  Album({this.id, this.userId, this.title});
+
+  // why use factory constructor?
+  //https://dart.dev/guides/language/language-tour#constructors
+  /*
+  Use the factory keyword when implementing a constructor that doesn’t always create a new instance of its class.
+   For example, a factory constructor might return an instance from a cache, or it might return an instance of a subtype.
+    ***Another use case for factory constructors is initializing a final variable
+    using logic that can’t be handled in the initializer list. ****
+
+    final variables do not have setters so you have to initialize them using factory constructors or using short hand this in normal constructors
+  * */
+  factory Album.fromJson(Map<String, dynamic> json){
+    return Album(
+      id: json['id'],
+      userId: json['userId'],
+      title: json['title']
+    );
+  }
+}
+
+class AlbumsResponse{
+  final List<Album> albums;
+
+  AlbumsResponse(this.albums);
+  factory AlbumsResponse.fromJson(List<dynamic> json){
+    return AlbumsResponse(List<Album>.from(json.map((e) => Album.fromJson(e))));
+  }
 }
