f70272ade6fb6c6d0227b2099fc47841d8869ff8
hazem ashraf <hazem.ashraf@ibtikar.net.sa>
2020-10-05 15:44:43 +0200

    [fix] fix import issue

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'file:///E:/android/projects/flutter-starter-project/lib/ui/main.dart';
+import 'package:flutterstarterproject/ui/main/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
